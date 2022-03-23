c04e7bd64b850538a98aead1c6ce47f8c45cea8c
Ricardo S. Sousa <93887680+RickSSousa@users.noreply.github.com>
2022-03-23 17:46:16 -0300

    feito associações com as classes

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -17,18 +17,22 @@ namespace proj_academia.Models
             if (!context.Alunos.Any())
             {
                 context.Alunos.AddRange(new Aluno { Nome = "Ricardo Santos", DataNasc = DateTime.Now, Email = "rick@gmail.com", Instagram = "@rics.exe", Obs = "iniciante", Tel = "(35)99866-8800", PersonalID = 1 });
+                context.SaveChanges();
             }
             if (!context.Personais.Any())
             {
                 context.Personais.AddRange(new Personal { Nome = "Leo Stronda", Especialidade = "musculação" });
+                context.SaveChanges();
             }
             if (!context.Exercicios.Any())
             {
                 context.Exercicios.AddRange(new Exercicio { Nome = "Supino", Categoria = "Peito", Descricao = "Levantamendo horizontal de barra não guiada" });
+                context.SaveChanges();
             }
             if (!context.Treinos.Any())
             {
                 context.Treinos.AddRange(new Treino { Data = DateTime.Now.Date, Hora = System.Convert.ToDateTime(DateTime.Now.Hour), AlunoID = 1 });
+                context.SaveChanges();
             }
         }
     }
